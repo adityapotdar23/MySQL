a654490c420f74a85a9a1f11d6807d30d46ff6ce
Aditya <91947505+adityapotdar23@users.noreply.github.com>
2023-07-10 15:29:29 +0530

    Create 23. Number of Unique Subjects Taught by Each Teacher.sql
    https://leetcode.com/problems/number-of-unique-subjects-taught-by-each-teacher/?envType=study-plan-v2&envId=top-sql-50

---
+++ LeetCode Study Plan 50/23. Number of Unique Subjects Taught by Each Teacher.sql
@@ -0,0 +1,20 @@
+-- Table: Teacher
+
+-- +-------------+------+
+-- | Column Name | Type |
+-- +-------------+------+
+-- | teacher_id  | int  |
+-- | subject_id  | int  |
+-- | dept_id     | int  |
+-- +-------------+------+
+-- (subject_id, dept_id) is the primary key for this table.
+-- Each row in this table indicates that the teacher with teacher_id teaches the subject subject_id in the department dept_id.
+ 
+
+-- Calculate the number of unique subjects each teacher teaches in the university.
+
+-- Return the result table in any order.
+
+-- # Write your MySQL query statement below
+SELECT teacher_id, COUNT(DISTINCT subject_id) AS cnt FROM Teacher 
+GROUP BY teacher_id
