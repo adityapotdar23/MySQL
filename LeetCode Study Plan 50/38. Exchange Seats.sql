4ebebe1e37a8de2e4855da6133ba4c607bc7fa32
Aditya <91947505+adityapotdar23@users.noreply.github.com>
2023-07-12 09:42:49 +0530

    Create 38. Exchange Seats.sql
    https://leetcode.com/problems/exchange-seats/description/

---
+++ LeetCode Study Plan 50/38. Exchange Seats.sql
@@ -0,0 +1,25 @@
+-- Table: Seat
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | id          | int     |
+-- | student     | varchar |
+-- +-------------+---------+
+-- id is the primary key column for this table.
+-- Each row of this table indicates the name and the ID of a student.
+-- id is a continuous increment.
+ 
+
+-- Write an SQL query to swap the seat id of every two consecutive students. If the number of students is odd, the id of the last student is not swapped.
+
+-- Return the result table ordered by id in ascending order.
+
+SELECT CASE
+        WHEN s.id%2<>0 AND s.id=(SELECT COUNT(*) FROM Seat) THEN s.id
+        WHEN s.id%2=0 THEN s.id - 1 
+        ELSE 
+            s.id + 1 
+        END AS id, student 
+FROM Seat s 
+ORDER BY id
