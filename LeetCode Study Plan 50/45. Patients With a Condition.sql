3d605378ea9ae7416e054ba540cbb49cd7746d08
Aditya <91947505+adityapotdar23@users.noreply.github.com>
2023-07-22 11:54:32 +0530

    Create 45. Patients With a Condition.sql
    https://leetcode.com/problems/patients-with-a-condition

---
+++ LeetCode Study Plan 50/45. Patients With a Condition.sql
@@ -0,0 +1,23 @@
+-- Table: Patients
+
+-- +--------------+---------+
+-- | Column Name  | Type    |
+-- +--------------+---------+
+-- | patient_id   | int     |
+-- | patient_name | varchar |
+-- | conditions   | varchar |
+-- +--------------+---------+
+-- In SQL, patient_id is the primary key for this table.
+-- 'conditions' contains 0 or more code separated by spaces. 
+-- This table contains information of the patients in the hospital.
+ 
+
+-- Find the patient_id, patient_name and conditions of the patients who have Type I Diabetes. Type I Diabetes always starts with DIAB1 prefix.
+
+-- Return the result table in any order. 
+
+-- # Write your MySQL query statement below 
+SELECT patient_id, patient_name, conditions
+FROM Patients 
+WHERE conditions REGEXP '\\bDIAB1'
+
